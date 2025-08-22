5c48b8fb41af189ec8d02f30a463a3bf3f00157b
maskgamer <gamermask64@gmail.com>
2025-08-22 18:08:18 +0530

    Removed .env file from repo and added to .gitignore

---
+++ 2_sequential_chain.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+import os
+
+os.environ['LANGCHAIN_PROJECT'] = "Sequential LLM chain"
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template="Generate a detailed report on {topic}",
+    input_variables=['topic']
+
+)
+
+
+prompt2 = PromptTemplate(
+    template="generate a 5 poointer summary from the following text \n {text}",
+    input_variables=['text']
+
+)
+
+
+model1 = ChatGroq(model="Gemma2-9b-It")
+model2 = ChatGroq(model="llama3-70b-8192")
+
+
+parser = StrOutputParser()
+
+chain = prompt1 | model1 | parser | prompt2 | model2 | parser
+
+result = chain.invoke({"topic":"Unemployment in India"})
+
+print(result)
